f1bb6af05688c1aec21b2573cde5ef99ad016924
varshasraut <55317808+varshasraut@users.noreply.github.com>
2023-10-25 12:23:32 +0530

    changes
    changes

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 
 <div class="container-fluid">
-    <form id="upload_file">
+    <form >
         <input type="hidden" name="reload" value="<?php echo $reload ?>">
         <div class="form-group">
             <div class="input-group">
                 <div class="custom-file">
-                    <input type="file" name="files[]" multiple accept="image/*,.pdf,video/*" class="custom-file-input" id="fileInput"  required>
+                    <input type="file" name="files[]" multiple accept="image/*,.pdf,video/*" class="custom-file-input" id="fileInput" onchange="filenames(this,$(this))" required>
                     <label class="custom-file-label" style="overflow:hidden;width:100%" for="fileInput">Choose file</label>
                 </div>
                 <div class="input-group-append">
