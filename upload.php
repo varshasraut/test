a22b78de73e2903616a5059cc25f4d11a8c9d9d6
varshasraut <55317808+varshasraut@users.noreply.github.com>
2023-10-25 12:20:45 +0530

    changes
    changes

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
         <div class="form-group">
             <div class="input-group">
                 <div class="custom-file">
-                    <input type="file" name="files[]" multiple accept="image/*,.pdf,video/*" class="custom-file-input" id="fileInput" onchange="filenames(this,$(this))" required>
+                    <input type="file" name="files[]" multiple accept="image/*,.pdf,video/*" class="custom-file-input" id="fileInput"  required>
                     <label class="custom-file-label" style="overflow:hidden;width:100%" for="fileInput">Choose file</label>
                 </div>
                 <div class="input-group-append">
